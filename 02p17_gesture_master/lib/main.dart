9c3eb1c2eb240eedda26a3c0f6991fbbc16be795
Eugene S. Ostrovsky <eso@mail.ru>
2023-06-19 21:04:09 +0300

    [02p17_gesture_master]: GestureMaster
    https://go.teachbase.ru/viewer/sessions/633782/tasks/283812

--- 02p17_gesture_master/lib/main.dart
+++ 02p17_gesture_master/lib/main.dart
@@ -11,10 +11,75 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: GestureMasterWidget(),
       ),
+    );
+  }
+}
+
+class GestureMasterWidget extends StatefulWidget {
+  const GestureMasterWidget({super.key});
+
+  @override
+  State<GestureMasterWidget> createState() => _GestureMasterWidgetState();
+}
+
+class _GestureMasterWidgetState extends State<GestureMasterWidget>
+    with SingleTickerProviderStateMixin {
+  static const squareSize = 80.0;
+  Color color = Colors.amber;
+  late final _controller = AnimationController(
+    duration: const Duration(seconds: 2),
+    vsync: this,
+  );
+  double x = 0;
+  double y = 0;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _rotateColor() {
+    setState(() {
+      final hsl = HSLColor.fromColor(color);
+      color = hsl.withHue((hsl.hue + 30) % 360).toColor();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          left: x,
+          right: -x,
+          top: y,
+          bottom: -y,
+          child: Center(
+            child: RotationTransition(
+              turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
+              child: Container(
+                color: color,
+                width: squareSize,
+                height: squareSize,
+              ),
+            ),
+          ),
+        ),
+        Positioned.fill(
+          child: GestureDetector(
+            onTap: _rotateColor,
+            onLongPress: () => _controller.repeat(),
+            onLongPressEnd: (details) => _controller.stop(),
+            onPanUpdate: (details) => setState(() {
+              x += details.delta.dx;
+              y += details.delta.dy;
+            }),
+          ),
+        ),
+      ],
     );
   }
 }
