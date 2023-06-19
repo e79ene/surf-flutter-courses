72de088c660658f2abe182cd179852a44f9f6859
Eugene S. Ostrovsky <eso@mail.ru>
2023-06-19 21:04:09 +0300

    [02p17_gesture_master]: GestureMaster
    https://go.teachbase.ru/viewer/sessions/633782/tasks/283812

--- 02p17_gesture_master/lib/main.dart
+++ 02p17_gesture_master/lib/main.dart
@@ -11,10 +11,76 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: GestureMasterWidget(),
       ),
+    );
+  }
+}
+
+class GestureMasterWidget extends StatefulWidget {
+  const GestureMasterWidget({super.key});
+
+  @override
+  State<GestureMasterWidget> createState() => _GestureMasterWidgetState();
+}
+
+class _GestureMasterWidgetState extends State<GestureMasterWidget>
+    with SingleTickerProviderStateMixin {
+  Color color = Colors.amber;
+  bool _alignTrigger = false;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 5000),
+      vsync: this,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _rotateColor() {
+    setState(() {
+      final hsl = HSLColor.fromColor(color);
+      color = hsl.withHue((hsl.hue + 30) % 360).toColor();
+    });
+  }
+
+  void _startMovement() {
+    setState(() => _alignTrigger = !_alignTrigger);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        GestureDetector(
+          onTap: _rotateColor,
+          onLongPress: () => _controller.forward(),
+          onLongPressEnd: (details) => _controller.stop(),
+          onHorizontalDragEnd: (details) => _startMovement(),
+          onVerticalDragEnd: (details) => _startMovement(),
+        ),
+        RotationTransition(
+          turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
+          child: AnimatedContainer(
+            duration: const Duration(seconds: 1),
+            alignment: _alignTrigger ? Alignment.centerLeft : Alignment.center,
+            child: Container(
+              color: color,
+              width: 80,
+              height: 80,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
